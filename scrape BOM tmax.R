0a10ae1fb4a2db1eabca7de04a925246450180d3
rickyv78 <ricky.vandongen@dbca.wa.gov.au>
2025-03-31 10:36:48 +0800

    new codes

---
+++ scrape BOM tmax.R
@@ -0,0 +1,67 @@
+library(raster)
+library(stringr)
+library(XML)
+library(lubridate)
+library(sf)
+library(httr)
+library(xml2)
+library(RCurl)
+library(gdalUtilities)
+library(terra)
+library(here)
+library(doParallel)
+
+# Set the base directory where all the subfolders are located
+base_folder <- "E:\\AGCD\\bomR"
+
+# precip calib 1 day
+
+dir.create(here("tmax-mean"))
+dir.create(here("tmax-mean", "01day"))
+
+server <- "http://opendap.bom.gov.au:8080/thredds/fileServer/agcd/tmax/mean/r005/01day/"
+
+yrs <- 1910:year(Sys.Date())
+i<- 1
+
+UseCores <- 10
+#Register CoreCluster
+cl <- makeCluster(UseCores, outfile="")
+registerDoParallel(cl)
+
+foreach(i = 1:length(yrs)) %dopar% {
+  library(httr)
+  library(xml2)
+  library(RCurl)
+  library(here)
+#for (i in 1:length(yrs)) {
+  dir.create(here("tmax-mean", "01day", yrs[i]), showWarnings = FALSE)
+  m <- 1
+  for (m in 1:12){
+    if(m < 10){
+      mnth <- paste0(0, m)
+    }else{
+      mnth <- m
+    }
+    d <- 1
+    for (d in 1:31){
+      if(d < 10){
+        dy <- paste0(0, d)
+      }else{
+        dy <- d
+      }
+      filename <- paste0(yrs[i], "/tmax_mean_r005_", yrs[i], mnth, dy, "_",  yrs[i], mnth, dy,".nc" ) 
+      destfile <- here("tmax-mean", "01day", filename)
+    
+      if (file.exists(destfile) == FALSE){
+        #url.name <- "http://opendap.bom.gov.au:8080/thredds/fileServer/agcd/vapourpres_h15/mean/r005/01day/1971/vapourpres_h15_mean_r005_19710101_19710101.nc"
+        url.name <- paste0(server, filename)
+        if(url.exists(url.name)) {
+          download.file(url.name, destfile, mode = "wb", method = "curl", extra = "--max-time 600")
+        } 
+      }
+    }
+  } 
+  cat(yrs[i], "is done\n")
+}
+stopCluster(cl)
--- scrape BOM vpres15.R
+++ scrape BOM vpres15.R
@@ -11,18 +11,17 @@ library(terra)
 library(here)
 library(doParallel)
 
-
 # Set the base directory where all the subfolders are located
 base_folder <- "E:\\AGCD\\bomR"
 
 # precip calib 1 day
 
-dir.create(here("precip"))
-dir.create(here("precip", "calib-01day"))
+dir.create(here("vapourpres15"))
+dir.create(here("vapourpres15", "01day"))
 
-server <- "http://opendap.bom.gov.au:8080/thredds/fileServer/agcd/precip/calib/r005/01day/"
+server <- "http://opendap.bom.gov.au:8080/thredds/fileServer/agcd/vapourpres_h15/mean/r005/01day/"
 
-yrs <- 1900:year(Sys.Date())
+yrs <- 1971:year(Sys.Date())
 i<- 1
 
 UseCores <- 10
@@ -30,13 +29,13 @@ UseCores <- 10
 cl <- makeCluster(UseCores, outfile="")
 registerDoParallel(cl)
 
-#foreach(i = 1:length(yrs)) %dopar% {
+foreach(i = 1:length(yrs)) %dopar% {
   library(httr)
   library(xml2)
   library(RCurl)
   library(here)
-for (i in 1:length(yrs)) {
-  dir.create(here("precip", "calib-01day", yrs[i]), showWarnings = FALSE)
+#for (i in 1:length(yrs)) {
+  dir.create(here("vapourpres15", "01day", yrs[i]), showWarnings = FALSE)
   m <- 1
   for (m in 1:12){
     if(m < 10){
@@ -51,10 +50,11 @@ for (i in 1:length(yrs)) {
       }else{
         dy <- d
       }
-      filename <- paste0(yrs[i], "/precip_calib_r005_", yrs[i], mnth, dy, "_",  yrs[i], mnth, dy,".nc" ) 
-      destfile <- here("precip", "calib-01day", filename)
+      filename <- paste0(yrs[i], "/vapourpres_h15_mean_r005_", yrs[i], mnth, dy, "_",  yrs[i], mnth, dy,".nc" ) 
+      destfile <- here("vapourpres15", "01day", filename)
     
       if (file.exists(destfile) == FALSE){
+        #url.name <- "http://opendap.bom.gov.au:8080/thredds/fileServer/agcd/vapourpres_h15/mean/r005/01day/1971/vapourpres_h15_mean_r005_19710101_19710101.nc"
         url.name <- paste0(server, filename)
         if(url.exists(url.name)) {
           download.file(url.name, destfile, mode = "wb", method = "curl", extra = "--max-time 600")
